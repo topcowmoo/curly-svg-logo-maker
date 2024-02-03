3b904db81bcb249fa6da189b84d6e6fc909bca89
Salvatore <mammoliti.sam@gmail.com>
2024-02-03 14:29:48 -0500

    "changed line 15 from shape to text in index.js. Shape was rending but not text. This isnt working either."

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Svg {
     return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
   }
   setTextElement(text, color) {
-    this.shapeElement = `<text x="150" y="150" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+    this.textElement = `<text x="150" y="150" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
   }
   setShapeElement(shape) {
     this.shapeElement = shape.render()
