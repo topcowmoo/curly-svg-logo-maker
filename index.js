2a8f209b93d99b1b407be43e9abb727f7fed6916
Salvatore <mammoliti.sam@gmail.com>
2024-02-07 14:02:36 -0500

    "fixed jest and node index.js errors by cleaning up code. Deleted element-method.test.js. Created svg.js file in lib folder and moved the svg class function from the index to svg.js. Created svg.test.js for jest testing."

--- index.js
+++ index.js
@@ -1,47 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const { Triangle, Circle, Square } = require("./lib/shapes");  // Importing the classes from the shapes module
-
-class Svg {
-  constructor() {
-    this.textElement = "";
-    this.shapeElement = "";
-  }
-
-  render() {
-    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
-  }
-
-  setTextElement(text, color, shape) {
-    let textX, textY;
-
-    switch (shape.toLowerCase()) {
-      case "triangle":
-        textX = 150;
-        textY = 130; // Adjusted for vertical centering
-        break;
-      case "circle":
-        textX = 150;
-        textY = 105;
-        break;
-      case "square":
-        textX = 150;
-        textY = 100;
-        break;
-      default:
-        console.log("Invalid shape!");
-        return;
-    }
-
-    this.textElement = `<text x="${textX}" y="${textY}" font-size="40" text-anchor="middle" dominant-baseline="middle" fill="${color}">${text}</text>`;
-  }
-
-  setShapeElement(shape) {
-    this.shapeElement = `<g transform="translate(${150 - shape.centerX()}, ${100 - shape.centerY()})">${shape.render()}</g>`;
-  }
-}
-
-module.exports = Svg; 
+const Svg = require("./lib/svg"); // Import Svg class from svg.js file
 
 const questions = [
   {
@@ -129,4 +89,4 @@ const init = async () => {
 };
 
 init();
-
+ 
